8fd868b48949579a5780dd42a9e7f5c6e127c9b0
stephenstc <292079443@qq.com>
2016-11-18 14:39:58 +0800

    Update mem_wb.vhd

--- mem_wb.vhd
+++ mem_wb.vhd
@@ -31,19 +31,33 @@ use work.HEADER.ALL;
 --use UNISIM.VComponents.all;
 
 entity mem_wb is
-    Port ( mem_op : in  STD_LOGIC_VECTOR(4 DOWNTO 0);
-           mem_aux : in  STD_LOGIC_VECTOR(7 DOWNTO 0);
+    Port ( op : in  operation;
            mem_data : in  INT16;
            mem_target_reg : in  STD_LOGIC_VECTOR(4 DOWNTO 0);
+			  rst : in  STD_LOGIC);
+			  clk : in  STD_LOGIC;
            wb_en : out  STD_LOGIC;
            wb_data : out  INT16;
            wb_target_reg : out  STD_LOGIC_VECTOR(4 DOWNTO 0));
 end mem_wb;
 
 architecture Behavioral of mem_wb is
-
 begin
-
-
+	process (clk, rst)
+	begin
+		if (rst = '0') then
+			wb_en <= '0';
+			wb_data <= "0000000000000000";
+			wb_target_reg <= "00000";
+		elsif ( clk'event and clk = '1' ) then
+			wb_data <= mem_data;
+			wb_target_reg <= mem_target_reg;
+			if ((op = B or op = BEQZ or op = BNEZ or op = BTEQZ or 
+				op = BEQZ or op = JR or op = JRRA or op = NOP or op = SW or op = SW_SP))
+				then wb_en <= '0';
+				else wb_en <= '1';
+			end if;
+		end if;
+	end process;
 end Behavioral;
 
